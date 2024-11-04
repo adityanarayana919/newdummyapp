06f1cf7f511d2f430097426f4d9527a1d9f36e7a
Aditya Narayana <adityanarayana919@gmail.com>
2024-11-04 15:18:54 +0100

    Initial Commit

---
+++ newdummyapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  newdummyapp
+//
+//  Created by Aditya Narayana on 04/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ newdummyapp/newdummyappApp.swift
@@ -0,0 +1,17 @@
+//
+//  newdummyappApp.swift
+//  newdummyapp
+//
+//  Created by Aditya Narayana on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct newdummyappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
