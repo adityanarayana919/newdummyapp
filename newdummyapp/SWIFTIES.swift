c9449431a9fb02267a4567495861030c3617d3c5
Aditya Narayana <adityanarayana919@gmail.com>
2024-11-04 16:26:26 +0100

    trying to commit.

---
+++ newdummyapp/SWIFTIES.swift
@@ -0,0 +1,31 @@
+//
+//  SWIFTIES.swift
+//  newdummyapp
+//
+//  Created by Aditya Narayana on 04/11/24.
+//
+
+//
+//  ContentView.swift
+//  newdummyapp
+//
+//  Created by Aditya Narayana on 04/11/24.
+//
+
+import SwiftUI
+
+struct SWIFTIES: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
